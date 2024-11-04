25c540d2ae100d3a731de048692606da220555c4
niquola <niquola@gmail.com>
2024-11-04 21:33:18 +0000

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+export interface TypeRef {
+    name: string;
+    package: string;
+}
+
+export interface Element {
+    type: TypeRef;
+    array?: boolean;
+}
+
+export interface Schema {
+    kind: 'resource' | 'complex-type' | 'primitive';
+    name: string;
+    package: string;
+    base?: TypeRef;
+    elements: { [key: string]: Element };
+}
+
+
+
 export function test(label: string): string {
     return label;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,31 @@
-import { test } from '../src/index';
+import * as cg from '../src/index';
 
 describe('ups', () => {
     it('should return hello message with the provided name', () => {
-        const result = test('hello');
+        const result = cg.test('hello');
         expect(result).toBe('hello');
     });
 }); 
+
+
+const schema: cg.Schema = {
+    kind: 'resource',
+    name: 'Patient',
+    package: 'fhir.r4',
+    base: {
+        name: 'Resource',
+        package: 'fhir.r4'
+    },
+    elements: {
+        name: {
+            array: true,
+            type: {
+                name: 'HumanName',
+                package: 'fhir.r4'
+            }
+        }
+    }
+};
+
+
+console.log(schema);
