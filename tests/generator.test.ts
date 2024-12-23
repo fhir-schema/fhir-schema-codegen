37e45e1ec2cffe538eba1c663302da09d1da990b
Vitaly Kravtsov <mail@krvital.pro>
2024-12-23 22:01:48 +0600

    test: update tests

--- tests/generator.test.ts
+++ tests/generator.test.ts
@@ -23,63 +23,6 @@ class TestGenerator extends Generator {
     }
 }
 
-describe('Generator helpers', () => {
-    let g: Generator;
-    const testOutputDir = path.join(__dirname, '../tmp/test-output');
-
-    beforeEach(() => {
-        g = new TestGenerator({ outputDir: testOutputDir });
-    });
-
-    afterEach(() => {
-    });
-
-    test('words', async () => {
-        expect(g.words('kebab-case')).toEqual(['kebab', 'case']);
-        expect(g.words('snake-case')).toEqual(['snake', 'case']);
-        expect(g.words('camelCase')).toEqual(['camel', 'Case']);
-        expect(g.words('PascalCase')).toEqual(['Pascal', 'Case']);
-
-        expect(g.words('Pas_cal-CaSe')).toEqual(['Pas', 'cal', 'Ca', 'Se']);
-    });
-
-    test('to camelCase', async () => {
-        expect(g.camelCase('kebab-case')).toEqual('kebabCase');
-        expect(g.camelCase('snake-case')).toEqual('snakeCase');
-        expect(g.camelCase('camelCase')).toEqual('camelCase');
-        expect(g.camelCase('PascalCase')).toEqual('pascalCase');
-
-        expect(g.camelCase('Pas_cal-CaSe')).toEqual('pasCalCaSe');
-    })
-
-    test('to kebab-case', async () => {
-        expect(g.kebabCase('kebab-case')).toEqual('kebab-case');
-        expect(g.kebabCase('snake-case')).toEqual('snake-case');
-        expect(g.kebabCase('camelCase')).toEqual('camel-case');
-        expect(g.kebabCase('PascalCase')).toEqual('pascal-case');
-
-        expect(g.kebabCase('Pas_cal-CaSe')).toEqual('pas-cal-ca-se');
-    })
-
-    test('to snake_case', async () => {
-        expect(g.snakeCase('kebab-case')).toEqual('kebab_case');
-        expect(g.snakeCase('snake-case')).toEqual('snake_case');
-        expect(g.snakeCase('camelCase')).toEqual('camel_case');
-        expect(g.snakeCase('PascalCase')).toEqual('pascal_case');
-
-        expect(g.snakeCase('Pas_cal-CaSe')).toEqual('pas_cal_ca_se');
-    })
-
-    test('to PascalCase', async () => {
-        expect(g.pascalCase('kebab-case')).toEqual('KebabCase');
-        expect(g.pascalCase('snake-case')).toEqual('SnakeCase');
-        expect(g.pascalCase('camelCase')).toEqual('CamelCase');
-        expect(g.pascalCase('PascalCase')).toEqual('PascalCase');
-
-        expect(g.pascalCase('Pas_cal-CaSe')).toEqual('PasCalCaSe');
-    })
-
-})
 
 describe('Generator', () => {
     let g: Generator;
@@ -116,4 +59,4 @@ describe('Generator', () => {
         expect(g.readFile("package.json")).toEqual(JSON.stringify({ name: "test", version: "0.0.1" }, null, 2));
     });
 
-}); 
+});
