f6484e0e2d6eca526bbc86a07b2078c213127de6
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-07-17 10:12:01 +0200

    Bump type-schema to 0.0.13
    Co-authored-by: Andrey Listopadov <andrew.listopadov@health-samurai.io>

--- src/utils/type-schema.ts
+++ src/utils/type-schema.ts
@@ -9,7 +9,7 @@ import { spawn } from 'node:child_process';
 
 const execAsync = promisify(exec);
 
-export const TYPE_SCHEMA_VERSION = '0.0.12';
+export const TYPE_SCHEMA_VERSION = '0.0.13';
 const BIN_DIR = 'tmp/bin';
 
 interface BinaryInfo {
