80f1f48e264713113edc1108e5cf8b82d6361f5d
MikhailArtemyev <grumblemisha@gmail.com>
2025-09-08 20:27:58 +0100

    [py] fixes, resolved issues with resource family validator, format

--- example/python/aidbox/hl7_fhir_r4_core/resource_families.py
+++ example/python/aidbox/hl7_fhir_r4_core/resource_families.py
@@ -10,26 +10,28 @@ from typing import Any, Annotated, List
 from pydantic import BeforeValidator, BaseModel, ValidationError
 from pydantic_core import ValidationError as PydanticCoreValidationError
 
+
 def to_snake_case(name: str) -> str:
-    # Insert underscore before each capital letter (except the first one)
-    s = re.sub(r'(?<!^)(?=[A-Z])', '_', name)
+    s = re.sub(r"(?<!^)(?=[A-Z])", "_", name)
     return s.lower()
+
 
 def module_exists(name: str) -> bool:
     """Checks if a module exists without importing it"""
     return importlib.util.find_spec(name) is not None
 
+
 def import_and_create_module(module_name: str, class_name: str) -> Any:
     """
-        Dynamically import a module and create an instance of a specified class.
+    Dynamically import a module and create an instance of a specified class.
 
-        Args:
-            module_name: String name of the module (e.g., 'aidbox.hl7_fhir_r4_core.patient')
-            class_name: String name of the class (e.g., 'Patient')
+    Args:
+        module_name: String name of the module (e.g., 'aidbox.hl7_fhir_r4_core.patient')
+        class_name: String name of the class (e.g., 'Patient')
 
-        Returns:
-            Instance of the specified class
-        """
+    Returns:
+        Instance of the specified class
+    """
     try:
         module = importlib.import_module(module_name)
         class_obj = getattr(module, class_name)
@@ -38,37 +40,42 @@ def import_and_create_module(module_name: str, class_name: str) -> Any:
     except (ImportError, AttributeError) as e:
         raise ImportError(f"Could not import {class_name} from {module_name}: {e}")
 
-def import_and_create_module_if_exists(module_name: str, class_name: str) -> Any:
+
+def import_and_create_module_if_exists(package: str, class_name: str) -> Any:
     """
-        Dynamically import a module and create an instance of a specified class if the module exists.
+    Dynamically import a module and create an instance of a specified class if the module exists.
 
-        Args:
-            module_name: String name of the module (e.g., 'aidbox.hl7_fhir_r4_core.patient')
-            class_name: String name of the class (e.g., 'Patient')
+    Args:
+        package: String name of the package (e.g., 'aidbox.hl7_fhir_r4_core')
+        class_name: String name of the class (e.g., 'Patient')
 
-        Returns:
-            Instance of the specified class or None if the module does not exist
-        """
+    Returns:
+        Instance of the specified class or None if the module does not exist
+    """
+    module_name = package + "." + to_snake_case(class_name)
     if module_exists(module_name):
         return import_and_create_module(module_name, class_name)
     else:
         return None
 
-def validate_and_downcast(resource_data: dict, package_list: List[str], family: List[str]) -> Any:
+
+def validate_and_downcast(
+    resource_data: dict[str, Any], package_list: List[str], family: List[str]
+) -> Any:
     """
-        Validates and downcasts ResourceFamily to the appropriate FHIR resource class
+    Validates and downcasts ResourceFamily to the appropriate FHIR resource class
 
-        Args:
-            resource_data: Input value (dict or BaseModel)
-            package_list: List of package names to search for resource classes
-            family: List of valid resource types (e.g., 'Group' or 'Patient')
+    Args:
+        resource_data: Input value (dict)
+        package_list: List of package names to search for resource classes (e.g., ['aidbox.hl7_fhir_r4_core', 'aidbox.hl7_fhir_r4_extras'])
+        family: List of valid resource types (e.g., 'Group' or 'Patient')
 
-        Returns:
-            Instance of the appropriate FHIR resource class
+    Returns:
+        Instance of the appropriate FHIR resource class
     """
 
     # Extract and validate resource type
-    resource_type = resource_data.get('resourceType')
+    resource_type = resource_data.get("resourceType")
     if not resource_type:
         raise ValueError("Missing 'resourceType' field in resource")
 
@@ -78,19 +85,16 @@ def validate_and_downcast(resource_data: dict, package_list: List[str], family: 
     # Dynamically import and instantiate the appropriate class
     target_class = None
     for package in package_list:
-        package_name = package + '.' + to_snake_case(resource_type)
-        target_class = import_and_create_module_if_exists(package_name, resource_type)
+        target_class = import_and_create_module_if_exists(package, resource_type)
         if target_class is not None:
             break
     if target_class is None:
-        raise ImportError(f"Could not find class for resourceType '{resource_type}' in packages {package_list}")
+        raise ImportError(
+            f"Could not find class for resourceType '{resource_type}' in packages {package_list}"
+        )
 
-    # Validate and downcast to the target class
-    try:
-        return target_class.model_validate(resource_data)
+    return target_class.model_validate(resource_data)
 
-    except (ValidationError, PydanticCoreValidationError) as e:
-        raise ValueError(f"Failed to validate {resource_type}: {str(e)}")
 
 packages = ['aidbox.hl7_fhir_r4_core']
 
