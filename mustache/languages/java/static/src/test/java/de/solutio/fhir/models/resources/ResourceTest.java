f4b4c8b20f0b3153d7d9419453441ba69d4176da
Julian Stitz <julian.stitz@solutio.de>
2025-11-05 10:15:24 +0100

    Enhance Mustache Java templates with stricter `JsonAutoDetect` visibility for serialization control, refine `List.copyOf` logic for optional arrays, and update `ResourceTest` to include nested input serialization.

--- mustache/languages/java/static/src/test/java/de/solutio/fhir/models/resources/ResourceTest.java
+++ mustache/languages/java/static/src/test/java/de/solutio/fhir/models/resources/ResourceTest.java
@@ -1,17 +1,23 @@
 package de.solutio.fhir.models.resources;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.solutio.fhir.models.complex_types.CodeableConcept;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ResourceTest {
 
     @Test
     public void json() throws JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
-        final Task task = Task.builder().intent(Task.TaskIntent.PLAN).status(Task.TaskStatus.REQUESTED).build();
+        final Task task = Task.builder()
+                .intent(Task.TaskIntent.PLAN)
+                .status(Task.TaskStatus.REQUESTED)
+                .addInput(List.of(Task.TaskInput.builder().type(CodeableConcept.builder().build()).build()))
+                .build();
         final String json = objectMapper.writeValueAsString(task);
         assertThat(json).isNotBlank();
         final Task read = objectMapper.readValue(json, Task.class);
