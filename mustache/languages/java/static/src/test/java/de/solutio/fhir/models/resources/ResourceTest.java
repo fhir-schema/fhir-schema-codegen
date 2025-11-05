acfa60a16741a9b52b0b530e79d318df042a6dd5
Julian Stitz <julian.stitz@solutio.de>
2025-11-05 17:44:05 +0100

    Update `ResourceTest` to refine description handling, add assertion for description equality, and adjust import order.

--- mustache/languages/java/static/src/test/java/de/solutio/fhir/models/resources/ResourceTest.java
+++ mustache/languages/java/static/src/test/java/de/solutio/fhir/models/resources/ResourceTest.java
@@ -1,12 +1,12 @@
 package de.solutio.fhir.models.resources;
 
+import static de.solutio.fhir.models.complex_types.CodeableConceptDTO.codeableConceptDtoBuilder;
 import static de.solutio.fhir.models.complex_types.ElementDTO.elementDtoBuilder;
-import static de.solutio.fhir.models.resources.TaskDTO.taskDtoBuilder;
+import static de.solutio.fhir.models.complex_types.ExtensionDTO.extensionDtoBuilder;
 import static de.solutio.fhir.models.resources.TaskDTO.TaskInputDTO.taskInputDtoBuilder;
 import static de.solutio.fhir.models.resources.TaskDTO.TaskIntentDTO;
 import static de.solutio.fhir.models.resources.TaskDTO.TaskStatusDTO;
-import static de.solutio.fhir.models.complex_types.CodeableConceptDTO.codeableConceptDtoBuilder;
-import static de.solutio.fhir.models.complex_types.ExtensionDTO.extensionDtoBuilder;
+import static de.solutio.fhir.models.resources.TaskDTO.taskDtoBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,6 +21,7 @@ class ResourceTest {
         final ObjectMapper objectMapper = new ObjectMapper();
         final TaskDTO task =
                 taskDtoBuilder()
+                        .description("some description")
                         .intent(TaskIntentDTO.PLAN)
                         .status(TaskStatusDTO.REQUESTED)
                         .input(taskInputDtoBuilder().type(codeableConceptDtoBuilder().build()).build())
@@ -32,12 +33,12 @@ class ResourceTest {
 
         final TaskDTO taskWithExtension =
                 task.toBuilder()
-                        .descriptionElement(
-                                elementDtoBuilder()
-                                        .extension(extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
-                                        .build())
+                        .descriptionElement(elementDtoBuilder()
+                                .extension(extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
+                                .build())
                         .addExtension(extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
                         .build();
+        assertThat(taskWithExtension.description()).isEqualTo(task.description());
         assertThat(taskWithExtension.intent()).isEqualTo(task.intent());
         assertThat(taskWithExtension.status()).isEqualTo(task.status());
         final String jsonWithExtension = objectMapper.writeValueAsString(taskWithExtension);
