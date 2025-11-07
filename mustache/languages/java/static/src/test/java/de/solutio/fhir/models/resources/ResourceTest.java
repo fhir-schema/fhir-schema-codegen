a880bb9a54cf389ad5653f99654cd047979a1259
Julian Stitz <julian.stitz@solutio.de>
2025-11-07 19:55:51 +0100

    Add reference handling in Mustache Java templates and `config.json`. Enhance `ResourceTest` with additional assertions and update `ResourceName` with utility methods for improved resource referencing.

--- mustache/languages/java/static/src/test/java/de/solutio/fhir/models/resources/ResourceTest.java
+++ mustache/languages/java/static/src/test/java/de/solutio/fhir/models/resources/ResourceTest.java
@@ -1,8 +1,8 @@
 package de.solutio.fhir.models.resources;
-
 import static de.solutio.fhir.models.complex_types.CodeableConceptDTO.codeableConceptDtoBuilder;
 import static de.solutio.fhir.models.complex_types.ElementDTO.elementDtoBuilder;
 import static de.solutio.fhir.models.complex_types.ExtensionDTO.extensionDtoBuilder;
+import static de.solutio.fhir.models.complex_types.ReferenceDTO.referenceDtoBuilder;
 import static de.solutio.fhir.models.resources.TaskDTO.TaskInputDTO.taskInputDtoBuilder;
 import static de.solutio.fhir.models.resources.TaskDTO.TaskIntentDTO;
 import static de.solutio.fhir.models.resources.TaskDTO.TaskStatusDTO;
@@ -11,46 +11,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.solutio.fhir.models.ResourceName;
+import de.solutio.fhir.models.complex_types.ReferenceDTO;
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
-
 class ResourceTest {
-
     @Test
-    public void json() throws JsonProcessingException {
+    public void test() throws JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
-
-        final TaskDTO task =
-                taskDtoBuilder()
-                        .description("some description")
-                        .authoredOn(
-                                LocalDateTime.now(),
-                                elementDtoBuilder()
-                                        .extension(
-                                                extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
-                                        .build())
-                        .intent(TaskIntentDTO.PLAN)
-                        .status(TaskStatusDTO.REQUESTED)
-                        .input(taskInputDtoBuilder().type(codeableConceptDtoBuilder().build()).build())
-                        .build();
+        final TaskDTO task = taskDtoBuilder()
+                .description("some description")
+                .requester(referenceDtoBuilder().reference("123").type(URI.create("Practitioner")).build())
+                .forReference(referenceDtoBuilder().reference("Patient/123").build())
+                .addInsurance(referenceDtoBuilder().reference(ResourceName.ORGANIZATION_DTO, "123").build())
+                .addBasedOn(referenceDtoBuilder().reference(ResourceName.TASK_DTO, UUID.randomUUID()).build())
+                .authoredOn(
+                        LocalDateTime.now(),
+                        elementDtoBuilder()
+                                .extension(
+                                        extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
+                                .build())
+                .intent(TaskIntentDTO.PLAN)
+                .status(TaskStatusDTO.REQUESTED)
+                .input(taskInputDtoBuilder().type(codeableConceptDtoBuilder().build()).build())
+                .build();
         assertThat(task.toString()).isNotBlank();
         final String json = objectMapper.writeValueAsString(task);
         assertThat(json).isNotBlank();
         final TaskDTO read = objectMapper.readValue(json, TaskDTO.class);
         assertThat(read).isEqualTo(task);
         assertThat(read.toString()).isNotBlank();
-
-        final TaskDTO taskWithExtension =
-                task.toBuilder()
-                        ._description(
-                                elementDtoBuilder()
-                                        .extension(
-                                                extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
-                                        .build())
-                        .extension(extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
-                        .build();
+        final TaskDTO taskWithExtension = task.toBuilder()
+                ._description(
+                        elementDtoBuilder()
+                                .extension(
+                                        extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
+                                .build())
+                .extension(extensionDtoBuilder().url(URI.create("https://www.google.com")).build())
+                .build();
         assertThat(taskWithExtension.description()).isEqualTo(task.description());
         assertThat(taskWithExtension.intent()).isEqualTo(task.intent());
         assertThat(taskWithExtension.status()).isEqualTo(task.status());
@@ -59,5 +61,9 @@ class ResourceTest {
         final TaskDTO readWithExtension = objectMapper.readValue(jsonWithExtension, TaskDTO.class);
         assertThat(readWithExtension).isEqualTo(taskWithExtension);
         assertThat(readWithExtension.toString()).isNotBlank();
+        assertThat(readWithExtension.forReference().flatMap(ReferenceDTO::findReferencedResourceName)).hasValue(ResourceName.PATIENT_DTO);
+        assertThat(readWithExtension.requester().flatMap(ReferenceDTO::findReferencedResourceName)).hasValue(ResourceName.PRACTITIONER_DTO);
+        assertThat(readWithExtension.insurance().map(List::getFirst).flatMap(ReferenceDTO::findReferencedResourceName)).hasValue(ResourceName.ORGANIZATION_DTO);
+        assertThat(readWithExtension.basedOn().map(List::getFirst).flatMap(ReferenceDTO::findReferencedResourceName)).hasValue(ResourceName.TASK_DTO);
     }
 }
